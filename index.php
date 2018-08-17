2d97d7ef606f723a47dbe261a200ac2ae8a1f1a1
pembrock <pembrock@gmail.com>
2018-08-17 15:30:44 +0300

    checking left/right user in group

--- index.php
+++ index.php
@@ -6,10 +6,28 @@ include "config.php";
 $bot = new \TelegramBot\Api\Client(Config::get('token'));
 $body = json_decode($bot->getRawBody(), true);
 
+//ob_flush();
+//ob_start();
+//print_r($body);
+//file_put_contents('var_dump.txt', ob_get_flush());
+
 $bot->command('start', function ($message) use ($bot) {
     $answer = 'Добро пожаловать!';
     $bot->sendMessage($message->getChat()->getId(), $answer);
 });
+
+//добавлен новый юзер
+if (isset($body['message']['new_chat_member'])) {
+    $user = "@" . $body['message']['new_chat_member']['username'];
+    $bot->sendMessage($body['message']['chat']['id'], 'Новый пользователь: ' . $user);
+}
+
+//удален юзер
+if (isset($body['message']['left_chat_member'])) {
+    $user = "@" . $body['message']['left_chat_member']['username'];
+    $bot->sendMessage($body['message']['chat']['id'], 'Удален пользователь: ' . $user);
+}
+
 $message = mb_strtolower($body['message']['text']);
 
 if ($message == 'ping') {
@@ -19,5 +37,14 @@ if ($message == 'ping') {
 if ($message == 'contact') {
     $bot->sendContact($body['message']['chat']['id'], '8(977)777-66-55', 'Borak Obama');
 }
+
+if ($message == 'круто') {
+    $bot->sendMessage($body['message']['chat']['id'], 'И ты это все сам сделал! Какой ты молодец!');
+}
+//$update = $bot->getUpdates();
+//ob_flush();
+//ob_start();
+//print_r($update->update->message);
+//file_put_contents('var_dump.txt', ob_get_flush());
 
 $bot->run();
